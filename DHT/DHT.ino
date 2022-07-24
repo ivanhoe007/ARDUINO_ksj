f99c2af29c1f35d9a92a54033601701649a56ceb
ivanhoe007 <108964338+ivanhoe007@users.noreply.github.com>
2022-07-24 10:30:12 +0900

    button
    completed

---
+++ DHT/DHT.ino
@@ -0,0 +1,24 @@
+#include<DHT.h>-------------
+
+#define DHTPIN A1
+#define DHTTYPE dht11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int h = dht.readHumidity();
+  int t = dht.readTemperature();
+
+
+  Serial.print("Humidity : ");
+  Serial.println(h);
+  Serial.print("Temperature : ");
+  Serial.print(t);
+
+  delay(1000);
+}
--- button/button.ino
+++ button/button.ino
@@ -1,5 +1,6 @@
 void setup()
 {
+  pinMode(6, OUTPUT);
   pinMode(8, INPUT_PULLUP);
   Serial.begin(9600);
 
@@ -11,11 +12,13 @@ void loop()
   if(digitalRead(8) == LOW)
   {
     Serial.println("0");
+    digitalWrite(6, HIGH);
   }
 
   else if(digitalRead(8) == HIGH)
   {
     Serial.println("1");
+    digitalWrite(6, LOW);
   }
 
 
